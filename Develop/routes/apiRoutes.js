71b8e1319f1db89282ac7eeff38ab6a7b13e9e7b
ltram16 <ltram16@gmail.com>
2023-03-14 22:54:33 -0700

    updated server.js and created apiRoutes.js

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req,res)=> {
+    // get all the notes from DB
+})
+
+router.post('/', (req,res)=> {
+    // add a note to the DB
+})
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// middlwares
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+app.use('/api/notes', apiRoutes);
+
+
+// API GET routes
+app.get('/notes', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 // spiun up server
 app.listen(PORT, ()=> console.log(`Your app is running at http://localhost:${PORT}`));
