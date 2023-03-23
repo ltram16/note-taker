02f09587c9509d7c6418cfd52db406348291d8b6
ltram16 <ltram16@gmail.com>
2023-03-22 20:42:26 -0700

    edited api get routes and deleted apiRoutes.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
+const notes = require('./db/db.json');
+const uid = require('uid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,13 +12,29 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-app.use('/api/notes', apiRoutes);
+// app.use('/api/notes', apiRoutes);
 
 
 // API GET routes
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+app.get('/api/notes', (req,res)=> {
+    res.json(notes)
+})
+
+// API POST routes
+app.post('/api/notes', (req,res)=> {
+    
+})
+
+// delete
+
 // spiun up server
 app.listen(PORT, ()=> console.log(`Your app is running at http://localhost:${PORT}`));
+
